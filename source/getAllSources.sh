cf8905760eb63b31a4ca702ad8a1853d18a486b7
Andy Hunt <andy.hunt@dremio.com>
2022-10-04 10:06:24 +0100

    Changed to scripts only

--- source/getAllSources.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-source utils/login.sh
-
-curl -X GET  -s -k -H 'Content-Type: application/json' -H "Authorization: $DREMIO_AUTH_TOKEN"  $DREMIO_BASE_PATH/api/v3/source?pretty
-echo
-
-
-
